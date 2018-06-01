72477fb793f678d4d5df97db9753ee3436e7429b
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-06-01 18:57:15 +0200

    pyspark for cluster

---
+++ run_shell.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright 2018 Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Parameters (put your file)
+fitsdir="hdfs://134.158.75.222:8020//lsst/LSST10Y"
+
+
+#cluster: 1 machine(executor= 18cores de 2 GB=36GB)
+master="spark://134.158.75.222:7077 "
+n_executors=9
+executor_cores=17
+executor_mem=30
+driver_mem=4
+
+
+total_mem=$(($driver_mem+$n_executors*executor_mem))
+ncores_tot=$(($n_executors*executor_cores))
+
+echo "#executors=$n_executors"
+echo "#cores used=$ncores_tot"
+echo "mem used= ${total_mem}G"
+opts="--driver-memory ${driver_mem}g --total-executor-cores ${ncores_tot} --executor-cores ${executor_cores} --executor-memory ${executor_mem}g "
+
+
+
+# Run it!
+cmd="pyspark $opts --jars lib/spark-fits_2.11-0.4.0.jar"
+echo $cmd
+eval $cmd
