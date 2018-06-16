e354ef7f0c6feab8c424fd345b2e1ce2b88e55bf
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-06-16 18:42:10 +0200

    write dt output file

--- scripts/colore_ana.scala
+++ scripts/colore_ana.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 import org.apache.spark.sql.SQLContext
+import scala.collection.mutable.ArrayBuffer
 
 import java.io._
 
@@ -10,30 +11,35 @@ val f="/home/plaszczy/fits/galbench_srcs_s1_0.fits"
 
 //timer
 class Timer (var t0:Double=System.nanoTime().toDouble,   var dt:Double=0)  {
-  def step(){
+  def step:Double={
     val t1 = System.nanoTime().toDouble
     dt=(t1-t0)/1e9
     t0=t1
+    dt
+  }
+  def print(msg:String):Unit={
+    val sep="----------------------------------------"
+    println("\n"+msg+": "+dt+" s\n"+sep)
   }
 }
 
 
 val timer=new Timer
-//var ddt=Array[Double]()
+var ddt=ArrayBuffer[Double]()
 
 var ana="load(HDU)"
 val galraw=spark.read.format("com.sparkfits").option("hdu",1).load(f).select($"RA",$"Dec",($"Z_COSMO"+$"DZ_RSD").as("z"))
 
 galraw.printSchema
-val sep="----------------------------------------"
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
+
 
 ana="PZ + show(5)"
 val gal=galraw.withColumn("zrec",(galraw("z")+lit(0.03)*(lit(1)+galraw("z"))*randn()).cast(FloatType))
 gal.show(5)
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 //cache
 //gal.repartition(10000).rdd.getNumPartitions
@@ -41,19 +47,19 @@ println(ana+timer.dt+" s"+"\n"+sep)
 ana="cache (count)"
 val N=gal.cache.count
 println("Ntot="+N)
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 //stat
 ana="statistics z"
 gal.describe("z").show()
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 //ana="statistics all"
 gal.describe().show
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 ana="minmax"
 val minmax=gal.select(min("z"),max("z")).first()
@@ -61,16 +67,16 @@ val zmin=minmax(0).toString.toDouble
 val zmax=minmax(1).toString.toDouble
 val Nbins=100
 val dz=(zmax-zmin)/Nbins
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 ana="histo df"
 val zbin=gal.select($"z",(((gal("z")-lit(zmin+dz/2)))/dz).cast(IntegerType).as("bin"))
 val h=zbin.groupBy("bin").count.sort("bin")
 val p=h.select($"bin",(lit(zmin+dz/2)+h("bin")*lit(dz)).as("zbin"),$"count").drop("bin")
 val c=p.collect
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 ana="histo UDF"
 val sqlContext = SQLContext.getOrCreate(sc)
@@ -80,8 +86,8 @@ val h=zbin.groupBy("bin").count.sort("bin")
 val p=h.select($"bin",(lit(zmin+dz/2)+h("bin")*lit(dz)).as("zbin"),$"count").drop("bin")
 val c=p.collect
 
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 //histoRDD
 //ana="histo RDD countbykey"
@@ -95,11 +101,10 @@ val h=zbin.select("bin").rdd.map(r=>(r(0),1)).reduceByKey(_+_).sortBy(c => c._2,
 //.map(x=>(zmin+dz/2+x._1*dz,x._2)
 h.collect
 
-timer.step
-println(ana+timer.dt+" s"+"\n"+sep)
+ddt+=timer.step
+timer.print(ana)
 
 ana="RDD histogram"
-
 //save to csv
 //p.repartition(1).write.csv("histo.csv")
 //p.write.json("histo.json")
@@ -108,8 +113,7 @@ ana="RDD histogram"
 //java
 val file = "scala_perf.txt"
 val append=true
-//val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file,append))) 
-//for (x <- c) {
-//  writer.write(x(0).toString+"\t"+x(1).toString+"\n")
-//}
-//writer.close
+val writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file,append))) 
+for (x <- ddt) writer.write(x.toString+"\t")
+writer.write("\n")
+writer.close
