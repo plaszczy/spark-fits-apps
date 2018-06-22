edcdaa9ba50560d55fce1ba2d2223a93fb6f397b
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-06-22 19:09:26 +0200

    nicer

--- perf.py
+++ perf.py
@@ -12,18 +12,18 @@ def reject_outliers(data, m = 2.):
 labels=["load(HDU)","PZ + show(5)","cache (count)","statistics z","statistics all","minmax","histo df","histo UDF","histo pandas UDF"]
 
         
-tsca=genfromtxt("scala_perf.txt.save")
+tsca=genfromtxt("scala_perf.txt")
 tsca=np.transpose(tsca)
 
-tpy=genfromtxt("python_perf.txt.save")
+tpy=genfromtxt("python_perf.txt")
 tpy=np.transpose(tpy)
 
 figure(figsize=(15,10))
 for i in range(len(tpy)):
     subplot(3,3,i+1)
     #data=reject_outliers(t[i],5)
-    hist([tpy[i],tsca[i]],30)
-    title(labels[i])
+    hist([tpy[i],tsca[i]],30,label=labels[i])
+    legend()
     print(r"{}: ${:2.1f}\pm{:2.1f}$ & ${:2.1f}\pm{:2.1f}$".format(i+1,tpy[i].mean(),tpy[i].std(),tsca[i].mean(),tsca[i].std()))
 
 tight_layout()
