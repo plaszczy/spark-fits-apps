7f5528cfa95f4edd5319584c602f251e2f1595af
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-06-23 17:09:54 +0200

    tomo

--- scripts/tomo.py
+++ scripts/tomo.py
@@ -76,16 +76,21 @@ timer.step()
 timer.print("data loaded")
 ####
 
-#zshell=[0.0,0.1276595744680851,0.27161611588954276,0.433950088130761,0.6170075461900071,0.8234340414483059,1.0562128552502175,1.3187081133672667,1.6147134044354285,1.9485066050016535,2.324911703512503]
-zshell=[0.1,0.2,0.3,0.4,0.5]
+zshell=[0.0,0.1276595744680851,0.27161611588954276,0.433950088130761,0.6170075461900071,0.8234340414483059,1.0562128552502175,1.3187081133672667,1.6147134044354285,1.9485066050016535,2.324911703512503]
+#zshell=[0.1,0.2,0.3,0.4,0.5]
 
 #writemap
+write=True
+dt=[]
 for i in range(len(zshell)-1):
     z1=zshell[i]
     z2=zshell[i+1]
     print("shell=[{},{}]".format(z1,z2))
     map=benchmark(gal,z1,z2)
-    f=open("tomo_python.txt","a")
-    f.write(str(timer.step)+"\n")
-    f.close()
-    timer.print("done in ")
+    dt.append(timer.step())
+    if write:
+        hp.write_map("map{}.fits".format(i), map)
+ddt=np.array(dt)
+with open("tomo_python.txt", 'ab') as abc:
+    np.savetxt(abc, ddt.reshape(1,ddt.shape[0]))
+    
