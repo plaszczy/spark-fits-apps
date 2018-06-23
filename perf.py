7b7f05eb5e18bcce4e4126229021b17ed87fa817
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-06-23 13:20:36 +0200

    perf

--- perf.py
+++ perf.py
@@ -9,22 +9,21 @@ def reject_outliers(data, m = 2.):
 
 
 
-labels=["load(HDU)","PZ + show(5)","cache (count)","statistics z","statistics all","minmax","histo df","histo UDF","histo pandas UDF"]
+labels=["load(HDU)","PZ + show(5)","cache (count)","stat(z)","stat(all)","minmax(z)","histo dataframe","histo UDF","histo pandas UDF"]
 
         
 tsca=genfromtxt("scala_perf.txt")
 tsca=np.transpose(tsca)
 
-tpy=genfromtxt("python_perf.txt")
+tpy=genfromtxt("python_perf4.txt")
 tpy=np.transpose(tpy)
 
 figure(figsize=(15,10))
 for i in range(len(tpy)):
     subplot(3,3,i+1)
-    #data=reject_outliers(t[i],5)
     hist([tpy[i],tsca[i]],30,label=labels[i])
     legend()
-    print(r"{}: ${:2.1f}\pm{:2.1f}$ & ${:2.1f}\pm{:2.1f}$".format(i+1,tpy[i].mean(),tpy[i].std(),tsca[i].mean(),tsca[i].std()))
+    print(r"{} & ${:2.1f}\pm{:2.1f}$ & ${:2.1f}\pm{:2.1f}$\\".format(labels[i],tpy[i].mean(),tpy[i].std(),tsca[i].mean(),tsca[i].std()))
 
 tight_layout()
 show()
