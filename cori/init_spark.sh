680afaaa3b2df3fe4b9598988b11b56fe83c6935
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-11-16 14:02:33 -0800

    put back the spark options

--- cori/init_spark.sh
+++ cori/init_spark.sh
@@ -28,17 +28,20 @@ master="--master $SPARKURL"
 #driver_mem=4
 
 #config NERSC (from Lisa G)
-#executor_cores=32
-#executor_mem=100
-#driver_mem=10
-#n_executors=$(($SLURM_JOB_NUM_NODES=2-1))
+executor_cores=32
+executor_mem=100
+driver_mem=10
+n_executors=$(($SLURM_JOB_NUM_NODES-1))
 
-#total_mem=$(($driver_mem+$n_executors*$executor_mem))
-#ncores_tot=$(($n_executors*$executor_cores))
-#export SPARKOPTS="$master --driver-memory ${driver_mem}g --total-executor-cores ${ncores_tot} --executor-cores ${executor_cores} --executor-memory ${executor_mem}g --jars $JARS"
+total_mem=$(($driver_mem+$n_executors*$executor_mem))
+ncores_tot=$(($n_executors*$executor_cores))
+echo "mem=${total_mem}"
+echo "cores=${ncores_tot}"
 
-export SPARKOPTS="$master --jars $JARS"
+export SPARKOPTS="$master --driver-memory ${driver_mem}g --total-executor-cores ${ncores_tot} --executor-cores ${executor_cores} --executor-memory ${executor_mem}g --jars $JARS"
 
-alias run_pyspark="shifter pyspark $SPARKOPTS"
+#export SPARKOPTS="$master --jars $JARS"
+
+alias run_pyspark="shifter pyspark $SPARKOPTS "
 
 type -all run_pyspark
