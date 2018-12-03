879d8c5665a69fd97fb7420bed00247addcf7a7e
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2018-12-03 16:28:31 +0100

    update to better names

--- df_tools.py
+++ df_tools.py
@@ -4,7 +4,7 @@ from pyspark.sql import functions as F
 def minmax(df,col):
     return df.select(F.min(col),F.max(col)).first()
 
-def hist_df(df,col,Nbins,bounds=None):
+def df_histo(df,col,Nbins,bounds=None):
 
     if (bounds==None) :
         m=minmax(df,col)
@@ -18,4 +18,4 @@ def hist_df(df,col,Nbins,bounds=None):
     dz=(zmax-zmin)/Nbins
     zbin=df.select(df[col],((df[col]-zmin-dz/2)/dz).cast(IntegerType()).alias('bin'))
     h=zbin.groupBy("bin").count().orderBy(F.asc("bin"))
-    return h.select("bin",(zmin+dz/2+h['bin']*dz).alias('zbin'),"count").drop("bin")
+    return h.select("bin",(zmin+dz/2+h['bin']*dz).alias('loc'),"count").drop("bin")
