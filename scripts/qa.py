32d3bcba83817af0c68a8abef23d9a25a889b667
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-02-13 20:20:57 +0100

    nicify

--- scripts/qa.py
+++ scripts/qa.py
@@ -1,19 +1,20 @@
-import os,sys
-sys.path.insert(0,os.path.join(os.getcwd(),".."))
-from df_tools import *
-import pandas as pd
-import numpy as np
-import healpy as hp
-
-import matplotlib.pyplot as plt
-plt.set_cmap('jet')
-
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 
+import os,sys
+sys.path.insert(0,os.path.join(os.getcwd(),".."))
+from df_tools import *
+
+import pandas as pd
+import numpy as np
+import healpy as hp
+import matplotlib.pyplot as plt
+plt.set_cmap('jet')
+
+
 nside=2048
-area=hp.nside2pixarea(nside, degrees=True)*3600
-resol= hp.nside2resol(nside,arcmin=True)
+pixarea=hp.nside2pixpixarea(nside, degrees=True)*3600
+reso= hp.nside2resol(nside,arcmin=True)
 #create the ang2pix user-defined-function. 
 #we use pandas_udf because they are more efficient
 @pandas_udf('int', PandasUDFType.SCALAR)
@@ -21,17 +22,16 @@ def Ang2Pix(ra,dec):
     return pd.Series(hp.ang2pix(nside,np.radians(90-dec),np.radians(ra)))
 
 
-
 def projmap(df):
     df_map=df.groupBy("ipix").count()
-    df_map=df_map.withColumn("dens",df_map['count']/area).drop("count")
+    df_map=df_map.withColumn("dens",df_map['count']/pixarea).drop("count")
     #statistics per pixel
     df_map.describe(['dens']).show() 
     #df_histplot(df_map,"dens",doStat=True)
     #back to python world
     map_p=df_map.toPandas()
-    A=map_p.index.size*area/3600
-    print("map area={} deg2".format(A))
+    A=map_p.index.size*pixarea/3600
+    print("map pixarea={} deg2".format(A))
     #now data is reduced create the healpy map
     map_c = np.full(hp.nside2npix(nside),hp.UNSEEN)
     map_c[map_p['ipix'].values]=map_p['dens'].values
@@ -63,15 +63,15 @@ timer=Timer()
 
 
 # input
+ff="/global/projecta/projectdirs/lsst/global/in2p3/Run1.2p/object_catalog/dpdd_object_run1.2p.parquet"
+print("reading {}".format(ff))
 timer.start()
-df_all=spark.read.parquet("/global/projecta/projectdirs/lsst/global/in2p3/Run1.2p/object_catalog/dpdd_object_run1.2p.parquet")
-
-df_all.printSchema()
+df_all=spark.read.parquet(ff)
+#df_all.printSchema()
 timer.stop()
 
-
 # build selection by appending to string
-cols=["tract","patch","ra","dec","good","clean","extendedness","blendedness","mag_i_cModel","snr_i_cModel"]
+cols=["tract","patch","ra","dec","good","clean","extendedness","blendedness","mag_i_cModel"]
 print(cols)
 #use these columns
 df=df_all.select(cols)
@@ -85,63 +85,54 @@ print("caching...")
 timer.start()
 df=df.cache()
 N=df.count()
-print("raw {}M objects".format(N/1e6))
+print("#raw objects ={}M".format(N/1e6))
 timer.stop()
 
+#stat nans
+for c in df.columns:
+    N_nans=num_nans(df,c)
+    print("#Nans in {}={}M ({:2.1f}%)".format(c,N_nans/1e6,float(N_nans/N)*100))
 
-#attention au nans
 
 #tract/patchs
 print("#tracts={}".format(df.select("tract").distinct().count())) #check that all the tracts are present
 df.groupBy(["tract","patch"]).count().groupBy("tract").count().\
 withColumnRenamed("count","#patches").sort("tract").show() 
 
-#stat nans
-print("NaNs stat:")
-for c in df.columns:
-    N_nans=N-df.select(c).na.drop().count()
-    print("{} : {:2.1f}% Nans".format(c,float(N_nans/N)*100))
-
-vals=["good","clean","extendedness"]
-for v in vals:
-      df.select(v).groupby(v).count().show(5)
-
-
+#density map
 dens_map=projmap(df)
-A=np.sum(dens_map!=hp.UNSEEN)*area/3600
-print("map area={} deg2".format(A))
 #plot
-hp.gnomview(dens_map,rot=[55,-29.8],reso=resol,min=100,max=400)
+hp.gnomview(dens_map,rot=[55,-29.8],reso=reso,min=100,max=400,title=r"density/$arcmin^2$")
 plt.show()
 #plt.savefig("newrun.png")
 
 ####
 #qual
+print("GALs good)
 dfqual=df.filter((df.good==True) & (df.clean==True) &(df.extendedness>0.9)) 
 Nqual=dfqual.count()
-print("dfqual N={} tot_frac={}".format(Nqual,Nqual/N))
-print("#nans={}".format(num_nans(dfqual)))
-
-
-print("qual stat:")
+print("#good={} ({2.1f}% raw)".format(Nqual,Nqual/N*100))
 for c in df.columns:
-    N_nans=Nqual-dfqual.select(c).na.drop().count()
-    print("{}: NANs={}M ({:2.1f}%)".format(c,N_nans/1e6,float(N_nans/Nqual)*100))
+    N_nans=num_nans(df,c)
+    print("#Nans in {}={}M ({:2.1f}%)".format(c,N_nans/1e6,float(N_nans/N)*100))
 
+######
 #with i band information
-dfqual_i=dfqual.select("ra","dec","mag_i_cModel","snr_i_cModel","ipix").na.drop()
+print("GALs with i band")
+dfqual_i=dfqual.select("ra","dec","mag_i_cModel","ipix").na.drop()
 Nqual_i=dfqual_i.count()
-print("dfqual N={} tot_frac={}".format(Nqual_i,Nqual_i/Nqual))
-print("#nans={}".format(num_nans(dfqual_i)))
+print("GAL_i N={}M tot_frac={}".format(Nqual_i))
+print("#nans={}M".format(num_nans(dfqual_i)/1e6)
 
-
+#######
 #i<24
+print("GALs i <24")
 df24=dfqual_i.filter(dfqual_i['mag_i_cModel']<24)
 df_map=df24.groupBy("ipix").count()
-df_map=df_map.withColumn("dens",df_map['count']/area).drop("count")
-print("density for i<24")
-df_map.describe().show()
+print("#gals i<24".format(df24.count())
+df_map=df_map.withColumn("dens",df_map['count']/pixarea).drop("count")
+df_map.describe(['dens']).show()
 
-
+A=np.sum(dens_map!=hp.UNSEEN)*pixarea/3600
 Nexp=40*10**(-0.36)
 print("exp number={}/sq-arcmin tot={}".format(Nexp,Nexp*A*3600))
