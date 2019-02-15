d18066ca1f6c54d5b8d8d3d98988dcabc2b26cb0
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-02-15 08:28:42 -0800

    add tracts outline

--- scripts/toast.py
+++ scripts/toast.py
@@ -22,6 +22,61 @@ def Ang2Pix(ra,dec):
     return pd.Series(hp.ang2pix(nside,np.radians(90-dec),np.radians(ra)))
 
 
+
+def tracts_outline(df):
+    #list of tracts
+    tracts=df.select("tract").distinct().toPandas()
+    df_withpix=df.groupBy(["tract","ipix"]).count().cache()
+    ipix=np.arange(hp.nside2npix(nside))
+    pixborder=[]
+    print("NSIDE={}".format(nside))
+    for t in tracts['tract'].values :
+        print("treating tract={}".format(t))
+        #create a map just for this tract
+        df_map=df_withpix.filter(df_withpix.tract==int(t))
+        #create the healpix map
+        tract_p=df_map.toPandas()
+        #plt.hist(tract_p['count'].values)
+        tract_map = np.full(hp.nside2npix(nside),hp.UNSEEN)
+        tract_map[tract_p['ipix'].values]=1
+        # for lit pixels compute the neighbours
+        ipix1=tract_p['ipix'].values
+        theta,phi=hp.pix2ang(nside,ipix1)
+        neighbours=hp.get_all_neighbours(nside,theta,phi,0).transpose()
+        # border if at least one neighbours is UNSEEN
+        mask=[(hp.UNSEEN in tract_map[neighbours[pixel]]) for pixel in range(len(ipix1))]
+        pixborder+=list(ipix1[mask])
+    return pixborder
+
+
+def get_borders_from_ipix(part):
+    # Get the pixel ID from the iterator
+    ipix = [*part]
+    if len(ipix) == 0:
+        # Empty partition
+        yield []
+    else:
+        # Get the 8 neighbours of all unique pixels.
+        theta, phi = hp.pix2ang(nside, ipix)
+        neighbours = hp.get_all_neighbours(nside, theta, phi, 0).flatten()
+
+        # Yield only pixels at the borders
+        unseen = np.array([i for i in neighbours if i not in ipix])
+        yield unseen
+
+
+def tracts_outline2(df):
+    #remove duplicate tract/ipix pairs
+    df_pix=df.groupBy(["tract","ipix"]).count().drop('count')
+    df_pix.show(5)
+    #reapartition according to tract
+    df_repart = df_pix.orderBy(df["tract"]).drop("tract").cache()
+    df_repart.show(5)
+    pix = df_repart.rdd.mapPartitions(get_borders_from_ipix).collect()
+    return pix
+
+
+
 def projmap(df,col,minmax=None):
     df_map=df.select(col,"ipix").na.drop().groupBy("ipix").avg(col)
     #statistics per pixel
@@ -41,6 +96,7 @@ def projmap(df,col,minmax=None):
         minmax=(np.max([0,mu-2*sig]),mu+2*sig)
     hp.gnomview(skyMap,rot=[55,-29.8],reso=reso,min=minmax[0],max=minmax[1],title=var)
     plt.show()
+    return skyMap
 
 def densitymap(df,minmax=None):
     df_map=df.select("ipix").groupBy("ipix").count()
@@ -82,9 +138,9 @@ print("reading {}".format(ff))
 df_all=spark.read.parquet(ff)
 
 
-cols="ra,dec,good,clean,extendedness"
-for b in ['u','g','r','i','z','y']:
-    s=",psFlux_{0},mag_{0},mag_{0}_cModel".format(b)
+cols="tract,ra,dec,good,clean"
+for b in ['u','i']:
+    s=",mag_{0}_cModel,snr_{0}_cModel".format(b)
     cols+=s
 print(cols)
 
@@ -92,7 +148,7 @@ print(cols)
 df=df_all.select(cols.split(','))
 
 #qual
-df=df.filter((df.good==True) & (df.clean==True) & (df.extendedness>0.9)) 
+df=df.filter((df.good==True) & (df.clean==True) ) 
 
 #add a column of healpix indices
 df=df.withColumn("ipix",Ang2Pix("ra","dec"))
