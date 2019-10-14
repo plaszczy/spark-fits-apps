d28d2e50d759601e94c8107b0d036d8c868b07c0
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-10-14 17:50:05 +0200

    ca avance

--- scripts/addhealpixels.scala
+++ scripts/addhealpixels.scala
@@ -6,9 +6,6 @@ import healpix.essentials.Scheme.NESTED
 
 import org.apache.spark.sql.functions.udf
 
-import scala.collection.mutable.ArrayBuffer
-
-
 
 class ExtPointing extends Pointing with java.io.Serializable
 case class Point2D(ra: Double, dec: Double)
@@ -35,31 +32,112 @@ val Ang2pix=spark.udf.register("Ang2pix",(theta:Double,phi:Double)=>grid.index(t
 
 val pix_neighbours=spark.udf.register("pix_neighbours",(ipix:Long)=>grid.neighbours(ipix))
 
+/************************/
+//SOURCE=run2
 
-//SOURCE
-val df_all=spark.read.parquet(System.getenv("RUN2")).select($"ra".as("ra_s"),$"dec".as("dec_s"),$"mag_i".as("mag_i_s"))
-//val df_all=spark.read.parquet("run2_extrait.parquet")
+val df_src=spark.read.parquet(System.getenv("RUN2"))
 
+// select columns
+var df=df_src.select("ra","dec","mag_i")
+
+//append _s
+for (n <- df.columns) df=df.withColumnRenamed(n,n+"_s")
+
+//val cols=List("ra","dec","mag_i")
+//val c=cols.map(c=> F.col(c).as(c+"_s"))
+//df_all=df_all.select(c: _*)
+
+//add id
+df=df.withColumn("id_s",F.monotonically_increasing_id)
 
 //filter
-val gold=df_all.filter($"mag_i_s"<25.3).withColumn("id_s",F.monotonically_increasing_id)
+df=df.filter($"mag_i_s"<25.3)
 
 
 //add healpixels
-val df=gold.withColumn("theta",F.radians(F.lit(90)-F.col("dec_s"))).withColumn("phi",F.radians("ra_s"))
-//ang2pix
-var source=df.withColumn("ipix",Ang2pix($"theta",$"phi")).drop("theta","phi")
+df=df.withColumn("theta",F.radians(F.lit(90)-F.col("dec_s"))).withColumn("phi",F.radians("ra_s"))
+val source=df.withColumn("ipix",Ang2pix($"theta",$"phi")).drop("theta","phi")
+
+val Ns=source.cache().count
+
+println(f"source size=${Ns/1e6}%3.2f M")
+
+
+/************************/
+//TARGET=cosmodc2
+
+val df_t=spark.read.parquet(System.getenv("COSMODC2"))
+// select columns
+df=df_t.select("ra","dec","mag_i","redshift")
+
+//append _t
+for (n <- df.columns) df=df.withColumnRenamed(n,n+"_t")
+
+//val cols=List("ra","dec","mag_i")
+//val c=cols.map(c=> F.col(c).as(c+"_s"))
+//df_all=df_all.select(c: _*)
+
+//add id
+df=df.withColumn("id_t",F.monotonically_increasing_id)
+
+//filter
+df=df.filter($"mag_i_t"<25.3)
+
+
+//add healpixels
+df=df.withColumn("theta",F.radians(F.lit(90)-F.col("dec_t"))).withColumn("phi",F.radians("ra_t"))
+df=df.withColumn("ipix",Ang2pix($"theta",$"phi")).drop("theta","phi")
+
 
 
 //add neighbours
-val dfpixn=source.withColumn("neighbours",pix_neighbours($"ipix"))
-//create duplicates and add to source
+val dfn=df.withColumn("neighbours",pix_neighbours($"ipix"))
+
+//create duplicates
+val dup=new Array[org.apache.spark.sql.DataFrame](9)
+
 for (i <- 0 to 7) {
   println(i)
-  val df1=dfpixn.drop("ipix").withColumn("ipix",$"neighbours"(i))
+  val df1=dfn.drop("ipix").withColumn("ipix",$"neighbours"(i))
   val dfclean1=df1.filter(not(df1("ipix")===F.lit(-1))).drop("neighbours")
-  source=source.union(dfclean1)
+  dup(i)=dfclean1
 }
 
+dup(8)=df
+
+val target=dup.reduceLeft(_.union(_))
+
+val Nt=target.cache().count
+
 //TARGET
-println("source=",source.cache().count)
+println(f"target size=${Nt/1e6}%3.2f M")
+
+
+//join by index: tous les candidats paires
+val matched=source.join(target,"ipix").drop("ipix")
+println(f"matched size=${matched.cache.count()/1e6}%3.2f M")
+
+
+//val rdd=matched.drop("ipix")
+
+//choix par source
+// combien de candidats a chaque fois
+val nass=matched.groupBy("id_s").count.withColumnRenamed("count","ncand")
+//stat nass
+nass.groupBy("ncand").count.sort("ncand").show
+
+
+//df : agg ou window?
+
+
+// pair RDD
+//find location of id_s
+val id=matched.columns.indexOf("id_s")
+val rdd=matched.rdd
+val rdd=matched.rdd.map(r=>(r.getLong(id),r))
+
+
+def bestcand(it:Iterable[org.apache.spark.sql.Row]):org.apache.spark.sql.Row = it.head
+
+
+val assoc=rdd.groupByKey.mapValues(bestcand)
