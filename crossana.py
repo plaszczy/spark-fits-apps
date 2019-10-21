76fb4c46b4f146c56b0f69db0d7bd71af297e059
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-10-21 09:49:12 +0200

    new

--- crossana.py
+++ crossana.py
@@ -3,32 +3,35 @@ from pylab import *
 from tools import *
 from scipy.interpolate import interp1d
 
+
+def histo_plot(x,y,label=""):
+    figure()
+    bar_outline(x,y)
+    ylim(0.8*min(y),1.2*max(y))
+    semilogy()
+    axvline(0,c='k',lw=0.5)
+    xlabel(label)
+
+    f=y/sum(y)
+    #moyenne
+    xmean=sum(x*f)
+    xx=x-xmean
+    vx=sum(xx**2*f)
+
+    imax=argmax(y)
+    fup = interp1d(y[imax:],x[imax:])
+    x1=fup(y[imax]/2)
+    fd = interp1d(y[0:imax+1],x[0:imax+1])
+    x2=fd(y[imax]/2)
+
+    fwhm=float(x1)-float(x2)
+
+    stat=["N={:d}".format(int(sum(y))),r"$\mu={:g}$".format(xmean),r"$\sigma={:g}$".format(sqrt(vx)),"fwhm={:g}".format(fwhm)]
+    ax=gca()
+    text(0.6,0.8,"\n".join(stat), horizontalalignment='left',transform=ax.transAxes)
+    show()
+
 x,y=loadtxt("df.txt",unpack=True)
-figure()
-bar_outline(x,y)
-ylim(0.8*min(y),1.2*max(y))
-semilogy()
-axvline(0,c='k',lw=0.5)
-xlabel(r"$\Delta\theta\quad(arcsec)$")
-
-f=y/sum(y)
-
-#moyenne
-xmean=sum(x*f)
-xx=x-xmean
-vx=sum(xx**2*f)
-
-imax=argmax(y)
-fup = interp1d(y[imax:],x[imax:])
-x1=fup(y[imax]/2)
-fd = interp1d(y[0:imax+1],x[0:imax+1])
-x2=fd(y[imax]/2)
-
-fwhm=float(x1)-float(x2)
+histo_plot(x,y)
 
 
-stat=["N={:d}".format(int(sum(y))),r"$\mu={:g}$".format(xmean),r"$\sigma={:g}$".format(sqrt(vx)),"fwhm={:g}".format(fwhm)]
-ax=gca()
-text(0.6,0.8,"\n".join(stat), horizontalalignment='left',transform=ax.transAxes)
-
-show()
--- crossana.scala
+++ crossana.scala
@@ -6,12 +6,12 @@ df_hist(df1.withColumn("dphi",F.degrees($"phi_s"-$"phi_t")*3600),"dphi",Some(-5.
 
 df_hist(df1.withColumn("dtet",F.degrees($"theta_s"-$"theta_t")*3600),"dtet",Some(-5.0,5.0),Nbins=1000,fn="dtet.txt")
 
-df_hist(df1.withColumn("dphipull",F.degrees($"phi_s"-$"phi_t")*3600/$"sigr"),"dphipull",Some(-15.0,15.0),Nbins=1000,fn="dphipull.txt")
+df_hist(df1.withColumn("dphipull",F.degrees($"phi_s"-$"phi_t")*3600/$"sigr"),"dphipull",Some(-15.0,15.0),Nbins=1000,fn="phipull.txt")
 
-df_hist(df1.withColumn("dtetpull",F.degrees($"theta_s"-$"theta_t")*3600/$"sigr"),"dtetpull",Some(-15.0,15.0),Nbins=1000,fn="dtetpull.txt")
+df_hist(df1.withColumn("dtetpull",F.degrees($"theta_s"-$"theta_t")*3600/$"sigr"),"dtetpull",Some(-15.0,15.0),Nbins=1000,fn="tetpull.txt")
 
-df_hist(df1.withColumn("dmag",$"mag_i_cModel"-$"mag_i"),"dmag",fn="dmag.txt")
+df_hist(df1.withColumn("dmag",$"mag_i_cModel"-$"mag_i"),"dmag",fn="dmag.txt",Nbins=1000)
 
 //cosmodc2 flux
 val df2=df1.withColumn("flux_i",F.pow(F.lit(10),(F.lit(31.4)-$"mag_i")/2.5))
-df_hist(df2.withColumn("dflux",$"cModelFlux_i"-$"flux_i"),"dflux",fn="dflux.txt")
+df_hist(df2.withColumn("dflux",$"cModelFlux_i"-$"flux_i"),"dflux",fn="dflux.txt",Nbins=1000)
