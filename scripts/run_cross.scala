26fc8fd06f42607960c57b4a2b106065e90dfdbd
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-12-06 06:55:15 -0800

    add cross example

--- scripts/run_cross.scala
+++ scripts/run_cross.scala
@@ -13,35 +13,37 @@ Logger.getLogger("akka").setLevel(Level.OFF)
 
 
 
+
+
+
 //COMMON CUTS
 val magcut=25.3
 
+//GALCAT cosmodc2
+var gal=spark.read.parquet(System.getenv("COSMODC2"))
+val cols=Array("galaxy_id","ra","dec","mag_i","mag_r")
 
-val obj_cols=Array("objectId","ra","dec","mag_i_cModel","psf_fwhm_i","magerr_i_cModel","cModelFlux_i","cModelFluxErr_i","clean","snr_i_cModel","blendedness","extendedness")
-
-val cosmo_cols=Array("galaxy_id","ra","dec","mag_i")
-
-val star_cols=Array("simobjid","ra","decl","rmag","imag"
-
-
-//SOURCE=run2
-val obj=spark.read.parquet(System.getenv("RUN2"))
-
-val stars=spark.read.format("fits").option("hdu",1).load("all_stars_DC2_run2.1i.fits")
-
-// select columns
-var df=src.select(cols).na.drop
-
+gal=gal.select(cols.head,cols.tail: _*).na.drop
 //filter
-df=df.filter($"mag_i_cModel"<magcut)
+gal=gal.filter($"mag_i"<magcut)
 
+/*
+//STARS
+var stars=spark.read.format("fits").option("hdu",1).load("all_stars_DC2_run2.1i.fits")
+val cols=Array("simobjid","ra","decl","rmag","imag")
+stars=stars.select(cols.head,cols.tail: _*).na.drop
+//cut?
+*/
 
-/************************/
-//TARGET=cosmodc2
+//OBJECT CAT
+var obj=spark.read.parquet(System.getenv("RUN2"))
+val cols=Array("objectId","ra","dec","psf_fwhm_i","mag_i_cModel","magerr_i_cModel","cModelFlux_i","cModelFluxErr_i","mag_r_cModel","magerr_r_cModel","clean","snr_i_cModel","blendedness","extendedness")
 
-val gal=spark.read.parquet(System.getenv("COSMODC2"))
-// select columns
-df=df_t.select("galaxy_id","ra","dec","mag_i").na.drop
-
+obj=obj.select(cols.head,cols.tail: _*).na.drop
 //filter
-df=df.filter($"mag_i"<magcut)
+obj=obj.filter($"mag_i_cModel"<magcut)
+
+
+
+///:load scripts/cross-tools.scala
+val df1=single_match(obj,gal)
