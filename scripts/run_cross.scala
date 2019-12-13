fa35e6d0de216e6eece6d7feac2f1612c515d1f0
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2019-12-13 04:31:37 -0800

    update stars mieux

--- scripts/run_cross.scala
+++ scripts/run_cross.scala
@@ -17,7 +17,7 @@ Logger.getLogger("akka").setLevel(Level.OFF)
 
 
 //COMMON CUTS
-val magcut=22.3
+val magcut=23.35
 
 //GALCAT cosmodc2
 var gal=spark.read.parquet(System.getenv("COSMODC2"))
@@ -30,13 +30,13 @@ gal=gal.filter($"mag_i"<magcut)
 
 //STARS
 var stars=spark.read.parquet("refcat_v3_dc2_r2p1i.parquet")
-val cols=Array("id","ra","dec","ra_smeared","dec_smeared","i","i_smeared","r","r_smeared")
+val cols=Array("id","ra","dec","ra_smeared","dec_smeared","i","i_smeared","r","r_smeared","isresolved","isagn")
 stars=stars.select(cols.head,cols.tail: _*).na.drop
 stars=stars.filter($"i_smeared"<magcut)
 
 //OBJECT CAT
 var obj=spark.read.parquet(System.getenv("RUN2"))
-val cols=Array("objectId","ra","dec","psf_fwhm_i","mag_i_cModel","magerr_i_cModel","cModelFlux_i","cModelFluxErr_i","mag_r_cModel","magerr_r_cModel","clean","snr_i_cModel","snr_r_cModel","blendedness","extendedness")
+val cols=Array("objectId","ra","dec","psf_fwhm_i","mag_i_cModel","magerr_i_cModel","cModelFlux_i","cModelFluxErr_i","mag_r_cModel","magerr_r_cModel","clean","snr_i_cModel","snr_r_cModel","blendedness","extendedness","psFlux_i","psFlux_flag_i","psFluxErr_i","psFlux_r","psFlux_flag_r","psFluxErr_r")
 
 obj=obj.select(cols.head,cols.tail: _*).na.drop
 //filter
