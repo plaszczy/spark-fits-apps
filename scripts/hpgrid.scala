b9b82e8c7bd98aa917b9afac1758535b05b07809
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2020-06-01 17:09:56 +0200

    add pix2ang

--- scripts/hpgrid.scala
+++ scripts/hpgrid.scala
@@ -3,9 +3,9 @@ import healpix.essentials.Pointing
 import healpix.essentials.Scheme.{NESTED,RING}
 
 
+//val hp = new HealpixBase(nside, NESTED)
 //Healpix
 class ExtPointing extends Pointing with java.io.Serializable
-//val hp = new HealpixBase(nside, NESTED)
 case class HealpixGrid(hp : HealpixBase, ptg : ExtPointing) {
 
   def index(theta : Double, phi : Double) : Long = {
@@ -16,10 +16,17 @@ case class HealpixGrid(hp : HealpixBase, ptg : ExtPointing) {
   def neighbours(ipix:Long):Array[Long] =  {
     hp.neighbours(ipix)
   }
+  def pix2ang(ipix:Long):Array[Double]=
+  {
+    val p:Pointing=hp.pix2ang(ipix)
+    Array(p.theta,p.phi)
+  }
 
 }
 /*
 val grid = HealpixGrid(new HealpixBase(nside, NESTED), new ExtPointing)
 val Ang2pix=spark.udf.register("Ang2pix",(theta:Double,phi:Double)=>grid.index(theta,phi))
 val pix_neighbours=spark.udf.register("pix_neighbours",(ipix:Long)=>grid.neighbours(ipix))
+val Pix2ang=spark.udf.register("Pix2ang",(ipix:Long)=> grid.pix2ang(ipix))
+
  */
