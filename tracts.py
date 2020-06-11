36e5a1f36378c5f4b555630b5d762056a9f7b5cd
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2020-06-11 16:24:40 +0200

    meilleur output tracts

--- tracts.py
+++ tracts.py
@@ -1,4 +1,9 @@
-df=df.select("tract","patch")
+ import pyspark.sql.functions as F
+
+df_all=spark.read.parquet(os.environ['RUN22'])
+
+df=df_all.select("tract","patch")
+df.cache().count()
 pairs=df.groupBy(["tract","patch"]).count()
 p= pairs.groupBy("tract").count() 
 p.count()
@@ -7,7 +12,7 @@ good.count()
 good.show(200)
 bad=p.filter(p['count']!=49).withColumnRenamed("count","#patches").sort("tract")
 ibad.show(200)
-pairs.join(bad,"tract").sort("patch").groupBy("tract").agg(F.collect_list("patch")).show(200,truncate=False)
+pairs.join(bad,"tract").sort("patch").groupBy("tract").agg(F.count("patch"),F.array_sort(F.collect_list("patch"))).show(200,truncate=False)
 
 
 
